e68e0467212e25f6bd9c7e0332f973d4a7cc1ef5
shohjahonochilov <shohjahonochilov405@gmail.com>
2023-03-28 15:25:19 +0300

    hello

---
+++ app.js
@@ -0,0 +1,47 @@
+// let book = {
+//     name : "JavaScrip",
+//     auther : "Hasan Shodiyev",
+//    sanasi  :" 29.08.2021 ",
+//      sahifalar : 730
+// }
+// console.log(book);
+
+
+// let computer = {
+//     name : 'HP',
+//     operatsion :"Windovis 11",
+//     ekiran : 15.6,
+//     protsessor : " Intel core 9 - 1030 H ",
+//     xotira : "16 Gb DDR 4 2933 МГц" ,
+//     vido_xotra : "NVIDIA GeForce GTX 1650 ",
+//    SSD :  "  512,ГБ " 
+// }
+// console.log(computer);
+
+let person = {
+    name : "shohjahon",
+    lastname: "ochilov",
+    profession :"IT dasturlash",
+    age : 15 ,
+    sex : "erkak",
+    mobile : 993422708 ,
+    addres : "Ishthon",
+
+    mather : {
+        name : "Shohnoza",
+    lastname: "Maxkkamova", 
+    prfession:"shifokor",
+    birth: "1983.11.07" ,
+ 
+    },
+    father :{
+        name : "Begzod",
+        lastname: "Begmatov", 
+        prfession:"тттттт",
+        birth: "1983.06.14" ,
+    }
+}
+console.log(person);
+
+
+
